863b5961189eb89a5056f9bceecbc121945d445e
Mikhel-lab <59959661+Mikhel-lab@users.noreply.github.com>
2020-04-21 09:49:55 +0100

    Update Program.cs

--- ShapeTest/Figure/Program.cs
+++ ShapeTest/Figure/Program.cs
@@ -7,8 +7,7 @@ namespace ShapeTest
         static void Main(string[] args)
         {
             
-            // Figure[] shapes = {new Triangle(5,3),
-            // new Rectangle(4,5), new Square(3,2), new Cube(6,4)};
+           
             Rectangle rec = new Rectangle(4,5, "rectangle");
             Triangle tri = new Triangle(5,3, "triangle");
             Square sql = new Square(3,2, "square");
@@ -17,13 +16,16 @@ namespace ShapeTest
             tri.Area();
             sql.Area();
             cb.Area();
+            Console.WriteLine(rec.Area());
+            Console.WriteLine(tri.Area());
+            Console.WriteLine(sql.Area());
+            Console.WriteLine(cb.Area());
             
 
-           
-       
 
         }
         
     }
 
 }
+
