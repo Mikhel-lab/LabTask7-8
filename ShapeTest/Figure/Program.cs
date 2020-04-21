e1761ff6df09ca70213c4fc0c929cccaaccd0fc3
Mikhel-lab <59959661+Mikhel-lab@users.noreply.github.com>
2020-04-21 09:35:06 +0100

    Update Program.cs

--- ShapeTest/Figure/Program.cs
+++ ShapeTest/Figure/Program.cs
@@ -1,29 +1,29 @@
-﻿using System;
+using System;
 
-namespace Figure
+namespace ShapeTest
 {
     class Program
     {    
         static void Main(string[] args)
         {
             
-            Figure[] shapes = {new Triangle(5,3),
-            new Rectangle(4,5), new Square(3,2), new Cube(6,4)};
+            // Figure[] shapes = {new Triangle(5,3),
+            // new Rectangle(4,5), new Square(3,2), new Cube(6,4)};
+            Rectangle rec = new Rectangle(4,5, "rectangle");
+            Triangle tri = new Triangle(5,3, "triangle");
+            Square sql = new Square(3,2, "square");
+            Cube cb = new Cube(6,4, "cube");
+            rec.Area();
+            tri.Area();
+            sql.Area();
+            cb.Area();
+            
 
            
-            foreach(shape s in Figure)
-            {
-                
-                s.GetInfo();
-                Console.WriteLine("{0} Area : {1:f2}",
-                s.Name, s.Area());
-                Console.WriteLine();
-            }
-            Console.ReadLine();
+       
 
         }
         
     }
 
 }
-
